82eeabe930414127da3349731e951fbee8dada53
PAZZO123 <smbabazipatrick@gmail.com>
2026-04-27 18:34:50 +0200

    Adding Question4

---
+++ Exercise4/fibbonacci.js
@@ -0,0 +1,15 @@
+function fibonacciUpTo(n) {
+  if (typeof n !== "number" || n < 0) return [];
+
+  const result = [0, 1];
+
+  while (true) {
+    const next = result[result.length - 1] + result[result.length - 2];
+    if (next > n) break;
+    result.push(next);
+  }
+
+  return n === 0 ? [0] : result;
+}
+
+console.log(fibonacciUpTo(15))
