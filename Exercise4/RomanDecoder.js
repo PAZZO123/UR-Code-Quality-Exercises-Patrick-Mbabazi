1bd785571bb040360c7b6bc8b77856a0a1c81fda
PAZZO123 <smbabazipatrick@gmail.com>
2026-04-27 18:06:20 +0200

    Adding Question2 A

---
+++ Exercise4/RomanDecoder.js
@@ -0,0 +1,36 @@
+/*
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered 
+"MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.*/
+
+function solution (roman) {
+   if (typeof roman !== "string" || roman.length === 0) return 0;
+
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = values[roman[i]];
+    const next = values[roman[i + 1]];
+
+    if (next && current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+}
+
+console.log(solution("MDCLXVI"))//->1666
